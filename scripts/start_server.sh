c8bbd67b1ae867038c960429e91e15a5bdc8b345
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-08-20 13:29:04 +0530

    update sh file

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:/usr/bin:/home/ec2-user/.npm-global/bin
 
 cd /var/www/todo
 
-npx prisma migrate deploy
+npx prisma migrate deploy --schema=prisma/schema.prisma
 
 # Stop proxy if running
 pm2 describe proxy > /dev/null 2>&1 && pm2 delete proxy || true
