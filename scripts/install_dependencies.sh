e7ce1fc197ee1c8e9abbc0f98a9ed67f9ed1f640
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-08-20 10:35:50 +0530

    update sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /var/www/todo
 
+# make sure ec2-user owns the code directory
+sudo chown -R ec2-user:ec2-user /var/www/todo
+
 # npm ci --production || true
 npm install --only=production
