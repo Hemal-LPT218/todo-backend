43d7fefce1f91e6b0f95fbae14d4a11822f47195
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-08-20 10:23:35 +0530

    update sh files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /var/www/todo
 
-# stop proxy server
-/usr/bin/pm2 delete proxy || true
-
-
 npm ci --production || true
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -xe
+
+export PATH=$PATH:/usr/bin:/home/ec2-user/.npm-global/bin
+
 cd /var/www/todo
 
-# stop existing pm2 app if any
-/usr/bin/pm2 delete todo || true
+# stop proxy server
+pm2 describe proxy > /dev/null 2>&1 && pm2 delete proxy || true
+
+# stop existing app if exists
+pm2 describe todo > /dev/null 2>&1 && pm2 delete todo || true
 
 # export env vars
 export $(grep -v '^#' .env | xargs)
