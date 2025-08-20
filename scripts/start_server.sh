d774a3c06555b4872987f3828ed0d4ba7249fd36
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-08-20 15:05:47 +0530

    update sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 set -xe
-export PATH=$PATH:/usr/bin:/home/ec2-user/.npm-global/bin
 
 cd /var/www/todo
 
 npx prisma migrate deploy --schema=prisma/schema.prisma
 
-# Stop proxy if running
-pm2 describe proxy > /dev/null 2>&1 && pm2 delete proxy || true
+export PM2_HOME=/home/ec2-user/.pm2
 
-# Stop old app
-pm2 describe todo > /dev/null 2>&1 && pm2 delete todo || true
+sudo -i -u ec2-user pm2 delete proxy || true
 
-# Load env vars
-export $(grep -v '^#' .env | xargs)
+sudo -i -u ec2-user pm2 delete todo || true
 
-# Start real app
-pm2 start dist/index.js --name todo --watch
-
-# Save PM2
-pm2 save
+sudo -i -u ec2-user pm2 start dist/index.js --name todo --watch
+sudo -i -u ec2-user pm2 save
