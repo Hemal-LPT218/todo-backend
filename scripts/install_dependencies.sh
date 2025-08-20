f2bd770e0cbfa97898bdb0709338554e2b9cb407
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-08-20 16:46:31 +0530

    change sh file

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -xe
+# set -xe
 
-export PATH=$PATH:/usr/bin:/home/ec2-user/.npm-global/bin
+# export PATH=$PATH:/usr/bin:/home/ec2-user/.npm-global/bin
 
 cd /var/www/todo
 
 # Fix permissions
-sudo chown -R ec2-user:ec2-user /var/www/todo
+# sudo chown -R ec2-user:ec2-user /var/www/todo
 
 # Install dependencies cleanly
 rm -rf node_modules
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -xe
+# set -xe
 
 cd /var/www/todo
 
 npx prisma migrate deploy --schema=prisma/schema.prisma
 
-export PM2_HOME=/home/ec2-user/.pm2
+# export PM2_HOME=/home/ec2-user/.pm2
 
-sudo pm2 delete proxy || true
+# sudo pm2 delete proxy || true
 
-sudo pm2 delete todo || true
+pm2 delete todo || true
 
 pm2 start dist/index.js --name todo --watch
 pm2 save
