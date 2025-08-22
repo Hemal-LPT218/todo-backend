58e2be6ddb20b11ae32595c8985318122f638f0d
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-08-22 10:03:17 +0530

    update server for corse

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,13 @@ import todosRouter from "./routes/todos.routes";
 export function createServer() {
   const app = express();
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: ["http://localhost:5173"], // allow frontend
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+    })
+  );
   app.use(express.json());
 
   // ---------- Logging Setup (Requests/Responses) ----------
