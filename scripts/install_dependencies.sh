4c2d022366236a4fff684d054a784ccfe7834ea8
Hemal Parmar <hemal.parmar@tatvasoft.com>
2025-09-04 18:34:25 +0530

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,12 +2,14 @@
 # set -xe
 
 # export PATH=$PATH:/usr/bin:/home/ec2-user/.npm-global/bin
-
+ # Create app directory
+mkdir -p /home/ec2-user/app
 cd /home/ec2-user/app
 
 # Fix permissions
+chown -R ec2-user:ec2-user /home/ec2-user/app
 # sudo chown -R ec2-user:ec2-user /var/www/todo
 
 # Install dependencies cleanly
-rm -rf node_modules
+# rm -rf node_modules
 npm install
